0bed4518391875a404c94d3fef8a6d166222c2f8
emna <gharsalliemna50@gmail.com>
2022-12-01 02:05:47 +0100

    second commit

--- website/auth.py
+++ website/auth.py
@@ -1,30 +1,64 @@
-from flask import Blueprint,render_template,request,flash,redirect,session
+from flask import Blueprint,render_template,request,flash,redirect,session,url_for
 from .models import User 
 from . import db
 from passlib.hash import pbkdf2_sha256
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         user = db.users.find_one ({ "email": request.form.get('email') })
         if user:
+            #check if the user with that email has a matching password in db with the one entered to login
             if pbkdf2_sha256.verify(request.form.get('password'), user['password']) :
                 flash('Logged in successfully',category='sucess')
                 User().start_session(user)
+                #login_user(user, remember=True)#remember that the user is logged in until he restarts his session
                 return redirect('/profile')
             else:
                 flash('Incorrect password,try again', category='error')
+                
         else:
             flash('email does not exist', category='error')   
     return render_template('login.html')
+
 
 @auth.route('/logout')
 def logout():
     session.clear()
     return redirect('/')
 
+
+@auth.route('/profile', methods=['GET','POST'])
+def profile():
+        return render_template('profile.html')
+ 
+ 
+@auth.route('/change_password', methods=['GET','POST'])
+def changepassword():
+    if request.method == 'POST':
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+        if password1 == "" or password2 == "" or password1 == password2:
+            print(session['user']['password'])
+            flash('Please refill the field',category='error')
+            return redirect('/change_password')
+        else:
+            new_password=pbkdf2_sha256.encrypt(password2)
+            db.users.update_one( {'email': session['user']['email']}, {'$set': {'password': new_password}})
+            flash('Password Changed Successfully',category='success')
+            return redirect('/profile')
+        #else:
+            #flash('Invalid Email',category='error')
+        
+    else:
+        return render_template('change_password.html')
+        
+        
 @auth.route('/signup', methods=['GET','POST'])
 def sign_up():
     if request.method == 'POST':
@@ -47,13 +81,12 @@ def sign_up():
         elif db.users.find_one ({ "email": email }):
             flash('Can not use this email.It already exists', category='error')
         else:
+            new_user=User()
             User().signup()
+            #login_user(new_user, remember=True)
             # add user to database
             flash ('A new account is created', category='success')
-            
+            return redirect('/profile')
        
     return render_template('sign-up.html')
 
-@auth.route('/profile', methods=['GET','POST'])
-def profile():
-    return render_template('profile.html')
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 #this file is defined as the blueprint of our app
 #it includes all the roots,urls needed
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,redirect,flash,session
+from . import db
+from passlib.hash import pbkdf2_sha256
 
 views = Blueprint(' views', __name__)
 
@@ -9,9 +11,6 @@ views = Blueprint(' views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/profile', methods=['GET','POST'])
-def profile():
-    return render_template('profile.html')
 
-
+        
 
